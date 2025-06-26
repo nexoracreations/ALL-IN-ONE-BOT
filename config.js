4c8874b19d9ca2421e973898750a298b6e05d8d2
nexoracreations <diogofikimarques@gmail.com>
2025-06-26 17:15:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-  ownerId: '1004206704994566164',
+  ownerId: '1084486083938619445',
   status: {
     rotateDefault: [
       { name: 'Netflix', type: ActivityType.Watching },
